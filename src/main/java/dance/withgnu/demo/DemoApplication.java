59273659160c9ed570777959c67fc4d44fa9d896
Choieeh <chyh0182@naver.com>
2024-06-04 22:42:32 +0900

    fix : test, main 수정 for 배포

--- src/main/java/dance/withgnu/demo/DemoApplication.java
+++ src/main/java/dance/withgnu/demo/DemoApplication.java
@@ -2,14 +2,13 @@ package dance.withgnu.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootApplication
-public abstract class DemoApplication {
+public class DemoApplication {
 	static {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
