75b0e879e07aa61775f6e47f3dcc1552e84fe9d8
Lee Geun Woo <gamebdy@naver.com>
2024-06-04 19:34:06 +0900

    Post List 정리

--- src/main/java/dance/withgnu/demo/user/controller/PoseController.java
+++ src/main/java/dance/withgnu/demo/user/controller/PoseController.java
@@ -4,8 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import dance.withgnu.demo.user.entity.Pose;
 @RestController
 @Tag(name = "API", description = "API List")
 @RequestMapping("/pose")
@@ -13,13 +14,16 @@ public class PoseController {
 
     @GetMapping("/list")
     @Operation(summary = "포즈 리스트", description = "포즈 리스트 path")
-    public String getList() {
-        return "Pose List";
+    public Pose getList(@RequestParam String x) {
+        // x 값을 사용하여 Pose 객체를 생성하고 반환합니다.
+        Pose pose = new Pose();
+        pose.setId(Long.parseLong(x));
+        pose.setMusicName("Music" + x);
+        return pose;
     }
-
 
     @GetMapping("/like")
     @Operation(summary = "좋아요", description = "좋아요 갯수")
-    public String getLike() { return "좋아요 갯수";}
+    public String getLike() { return "좋아요 갯수";} // 이건 일단 boolean 으로?
     // 기타 메서드 추가
 }
