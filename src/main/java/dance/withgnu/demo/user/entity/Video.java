7cd01c571c6437a9de99e6ec491dfaa9193a06bb
Choieeh <chyh0182@naver.com>
2024-06-05 11:20:09 +0900

    feat : community, dashboard 기능 추가

--- src/main/java/dance/withgnu/demo/user/entity/Video.java
+++ src/main/java/dance/withgnu/demo/user/entity/Video.java
@@ -4,11 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Builder;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Video {
 
@@ -24,85 +29,4 @@ public class Video {
     private Integer poseCategoryId; // 0 for full body, 1 for half body
     private String videoUrl;
     private LocalDateTime createDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getMusicName() {
-        return musicName;
-    }
-
-    public void setMusicName(String musicName) {
-        this.musicName = musicName;
-    }
-
-    public Integer getPoseNumber() {
-        return poseNumber;
-    }
-
-    public void setPoseNumber(Integer poseNumber) {
-        this.poseNumber = poseNumber;
-    }
-
-    public Integer getHeart() {
-        return heart;
-    }
-
-    public void setHeart(Integer heart) {
-        this.heart = heart;
-    }
-
-    public Integer getView() {
-        return view;
-    }
-
-    public void setView(Integer view) {
-        this.view = view;
-    }
-
-
-    public Integer getPoseCategoryId() {
-        return poseCategoryId;
-    }
-
-    public void setPoseCategoryId(Integer poseCategoryId) {
-        this.poseCategoryId = poseCategoryId;
-    }
-
-    public String getVideoUrl() {
-        return videoUrl;
-    }
-
-    public void setVideoUrl(String videoUrl) {
-        this.videoUrl = videoUrl;
-    }
-
-    public LocalDateTime getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(LocalDateTime createDate) {
-        this.createDate = createDate;
-    }
 }
--- src/main/java/dance/withgnu/demo/user/service/VideoService.java
+++ src/main/java/dance/withgnu/demo/user/service/VideoService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static dance.withgnu.demo.user.entity.Video.*;
 
@@ -32,6 +33,10 @@ public class VideoService {
         this.userRepository = userRepository;
         this.poseRepository = poseRepository;
         this.s3Service = s3Service;
+    }
+
+    public List<Video> getVideosExcludingUser(Long userId) {
+        return videoRepository.findByUserIdNot(userId);
     }
 
     @Transactional
