d922bdc0d14c8dcf8a50b4e9683595acc366bda6
Lee Geun Woo <gamebdy@naver.com>
2024-06-05 12:25:20 +0900

    DashboardService Conflict 에러 수정

--- src/main/java/dance/withgnu/demo/user/service/DashboardService.java
+++ src/main/java/dance/withgnu/demo/user/service/DashboardService.java
@@ -3,7 +3,6 @@ package dance.withgnu.demo.user.service;
 import dance.withgnu.demo.dto.UserDTO;
 import dance.withgnu.demo.dto.VideoListDTO;
 import dance.withgnu.demo.user.entity.UserEntity;
-import dance.withgnu.demo.user.entity.Video;
 import dance.withgnu.demo.user.repository.UserRepository;
 import dance.withgnu.demo.user.repository.VideoRepository;
 import jakarta.transaction.Transactional;
@@ -20,19 +19,12 @@ public class DashboardService {
     private final VideoRepository videoRepository;
 
     @Autowired
-<<<<<<< HEAD
     public DashboardService(UserRepository userRepository, VideoRepository videoRepository) {
         this.userRepository = userRepository;
         this.videoRepository = videoRepository;
     }
 
     @Transactional
-=======
-    private VideoRepository videoRepository;
-    public List<Video> getUserVideo(Long userId) {
-        return videoRepository.findByUserId(userId);
-    }
->>>>>>> 9aa3c7b33841b77efee50de970d869f3dbc89223
     public UserDTO getUserInfo(Long userId) {
         UserEntity user = userRepository.findByUserId(userId);
         if (user == null) {
@@ -49,7 +41,7 @@ public class DashboardService {
                         video.getPoseNumber(),
                         video.getHeart(),
                         video.getView(),
-                        video.getUserId(),
+                        video.getPoseId(),
                         video.getPoseCategoryId(),
                         video.getVideoUrl(),
                         video.getCreateDate()))
