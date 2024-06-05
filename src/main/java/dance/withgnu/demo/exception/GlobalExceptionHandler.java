0d0b5e38dcead7c1b57750d649081efae11e76ce
Lee Geun Woo <gamebdy@naver.com>
2024-06-05 12:17:06 +0900

    Handler 수정

--- src/main/java/dance/withgnu/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/dance/withgnu/demo/exception/GlobalExceptionHandler.java
@@ -5,12 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("{\"error\": \"" + ex.getMessage() + "\"}");
+    public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
